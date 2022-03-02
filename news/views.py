2538fd1ba7d8d2ddd9c5cbd66ad46ae774a27822
Kama <komaliyeva@gmail.com>
2022-03-02 18:46:03 +0600

    полная погинация

--- news/views.py
+++ news/views.py
@@ -13,7 +13,7 @@ def test(request):
     paginator = Paginator(objects, 2)
     page_num = request.GET.get('page', 1)
     page_objects = paginator.get_page(page_num)
-    return render(request, 'news/test.html', {'page_obj': page_objects} )
+    return render(request, 'news/test.html', {'page_obj': page_objects})
 
 
 class HomeNews(MyMixin, ListView):
@@ -38,6 +38,7 @@ class NewsByCategory(MyMixin, ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     allow_empty = False
+    paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
