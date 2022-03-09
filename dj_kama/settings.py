be7b213c47ebfe10d1184fd79ede7ecfe6138dda
Kama <komaliyeva@gmail.com>
2022-03-09 18:10:38 +0600

    postgres

--- dj_kama/settings.py
+++ dj_kama/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,11 +95,12 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django1',
+        'USER': 'postgres',
+        'PASSWORD': 'post111',
         'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
